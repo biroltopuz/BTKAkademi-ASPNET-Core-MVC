2940fda5fb1ef4bb98706f72fd21401b141dfafb
Birol Topuz <birolnet@gmail.com>
2024-07-17 18:26:20 +0300

    4.3 ders sonu

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,7 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/btk", () => "BTK Akademi");
+// app.MapGet("/", () => "Hello World!");
+// app.MapGet("/btk", () => "BTK Akademi");
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name:"default", 
+    pattern:"{controller=Home}/{action=Index}/{id?}");
 
 app.Run();

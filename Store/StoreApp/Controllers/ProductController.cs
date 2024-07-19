578129f6eabafe401b05e6f356d61cae1df3f637
Birol Topuz <birolnet@gmail.com>
2024-07-19 23:19:53 +0300

    4.8 ders sonu

---
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext _context;
+
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> IndexEski()
+        {
+            return _context.Products;
+
+            // var context = new RepositoryContext(
+            //     new DbContextOptionsBuilder<RepositoryContext>()
+            //     .UseSqlite("Data Source = D:\\GitHub\\BTKAkademi_ASPNET-Core-MVC\\ProductDb.db")
+            //     .Options
+            // );
+
+            // return context.Products;
+
+            // return new List<Product>()
+            // {
+            //     new Product(){ProductId=1, ProductName="Computer", Price=5}
+            // };
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Products.ToList();
+            return View(model);
+        }
+    }
+}

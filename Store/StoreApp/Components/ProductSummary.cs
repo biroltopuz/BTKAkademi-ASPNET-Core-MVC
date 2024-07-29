ddf2dc425f52f79702a90cfc22d5c7f788d552ba
Birol Topuz <birolnet@gmail.com>
2024-07-29 13:32:19 +0300

    8.2 ders sonu

---
+++ Store/StoreApp/Components/ProductSummary.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Repositories;
+using Services.Contracts;
+
+namespace StoreApp.Componenets
+{
+    public class ProductSummary : ViewComponent
+    {
+        private readonly IServiceManager _manager;
+
+        public ProductSummary(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public string Invoke(){
+            // service
+            return _manager.ProductService.GetAllProducts(false).Count().ToString();
+        }
+    }
+}

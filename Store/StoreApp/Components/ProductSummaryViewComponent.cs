f416cbe15bc2384ff2a759d12fddc5a07d8f447b
Birol Topuz <birolnet@gmail.com>
2024-07-29 14:20:36 +0300

    8.3 ders sonu

--- Store/StoreApp/Components/ProductSummaryViewComponent.cs
+++ Store/StoreApp/Components/ProductSummaryViewComponent.cs
@@ -4,11 +4,11 @@ using Services.Contracts;
 
 namespace StoreApp.Componenets
 {
-    public class ProductSummary : ViewComponent
+    public class ProductSummaryViewComponent : ViewComponent
     {
         private readonly IServiceManager _manager;
 
-        public ProductSummary(IServiceManager manager)
+        public ProductSummaryViewComponent(IServiceManager manager)
         {
             _manager = manager;
         }

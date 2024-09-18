bbfa4cdfaa91a4f89b9f8c6e0b5c2e0e28df1f56
Birol Topuz <birolnet@gmail.com>
2024-09-18 09:26:03 +0300

    16.11 ders sonu

--- Store/StoreApp/Areas/Admin/Controllers/UserController.cs
+++ Store/StoreApp/Areas/Admin/Controllers/UserController.cs
@@ -44,7 +44,19 @@ namespace StoreApp.Areas.Admin.Controllers
 
         public async Task<IActionResult> Update([FromRoute(Name = "id")] string id)
         {
-            // var user
+            var user = await _manager.AuthService.GetOneUserForUpdate(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update([FromForm] UserDtoForUpdate userDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _manager.AuthService.Update(userDto);
+                return RedirectToAction("Index");
+            }
             return View();
         }
 

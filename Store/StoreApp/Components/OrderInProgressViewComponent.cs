76618ecd3922ec1b0f5efa24803a18e5d85346ea
Birol Topuz <birolnet@gmail.com>
2024-09-21 22:38:08 +0300

    18.4 ders sonu

---
+++ Store/StoreApp/Components/OrderInProgressViewComponent.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace StoreApp.Componenets
+{
+    public class OrderInProgressViewComponent : ViewComponent
+    {        
+        private readonly IServiceManager _manager;
+
+        public OrderInProgressViewComponent(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public string Invoke()
+        {
+            return _manager
+                .OrderService
+                .NumberOfInProcess
+                .ToString();
+        }
+    }
+}

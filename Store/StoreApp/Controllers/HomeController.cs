2b1ce56edab57ab3ffebd3bb77f33c9920017a6f
Birol Topuz <birolnet@gmail.com>
2024-09-22 22:28:48 +0300

    18.7 ders sonu

--- Store/StoreApp/Controllers/HomeController.cs
+++ Store/StoreApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace StoreApp.Controllers{
     public class HomeController : Controller {
         public IActionResult Index(){
+            ViewData["Title"] = "Welcome";
             return View();
         }
     }
